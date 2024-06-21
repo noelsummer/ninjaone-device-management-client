00da17fed911bc442aab900ace02aa12e4452664
Noel Summerville <noelsummerdev@gmail.com>
2024-06-21 19:49:06 -0400

    fix (input): update component styles

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -29,17 +29,12 @@ export const Input = ({
   }
 
   return (
-    <div>
+    <div className={twMerge("w-full", className)}>
       <p className="mb-1">
         <span>{label}</span>
         {required && <span className="ml-1">*</span>}
       </p>
-      <div
-        className={twMerge(
-          "flex w-full items-center rounded border px-3 py-2 text-gray-600",
-          className,
-        )}
-      >
+      <div className="flex w-full items-center rounded border px-3 py-2 text-gray-600">
         {prefixIcon && <span className="mr-2">{prefixIcon}</span>}
         <input
           type="text"
